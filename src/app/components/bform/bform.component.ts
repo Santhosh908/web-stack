3a93ec5f63203657c75815439f6a587549610b3a
unknown <santhoshyavd@gmail.com>
2022-07-29 11:07:00 +0530

    home

--- src/app/components/bform/bform.component.ts
+++ src/app/components/bform/bform.component.ts
@@ -10,13 +10,18 @@ import { Router,RouterModule } from '@angular/router';
 export class BformComponent implements OnInit {
   a:String="Santhosh"
   b:String="abcd"
+  check:Boolean=false
   constructor(private router : Router) { 
   }
   ngOnInit(): void {
   }
-
+   setlogin(){
+    this.check=true
+  }
   submit(name:string,pass:string){
-    if(name==this.a && pass==this.b){
+    if(name==this.a && pass==this.b && this.check==true){
+      console.log("home")
+      console.log(this.check)
       this.router.navigate(['home']);
     }
     else{
--- src/app/components/homeheader/homeheader.component.ts
+++ src/app/components/homeheader/homeheader.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
+import { BformComponent } from '../bform/bform.component';
 
 @Component({
   selector: 'app-homeheader',
