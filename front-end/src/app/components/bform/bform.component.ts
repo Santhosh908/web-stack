a1560644db4d255261e059a5a5fa876760615e08
unknown <santhoshyavd@gmail.com>
2022-08-02 19:14:52 +0530

    changes

---
+++ front-end/src/app/components/bform/bform.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, NgForm } from '@angular/forms';
+import { Router,RouterModule } from '@angular/router';
+import { CanActivate } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-bform',
+  templateUrl: './bform.component.html',
+  styleUrls: ['./bform.component.css']
+})
+export class BformComponent implements OnInit {
+  a:String="Santhosh"
+  b:String="abcd"
+   Check:boolean
+  constructor(private router : Router,private http:HttpClient) { 
+    this.Check=false
+  }
+  ngOnInit(): void {
+  }
+  
+  submit(name:string,pass:string){
+    var formData: any = new FormData();
+    formData.append("name",name)
+    formData.append("pass",pass)
+    const url="http://localhost:3000/enroll"
+    this.http.post<any>(url,{"name":name,"pass":pass})
+    this.router.navigate(['home']);
+    
+    if(name=="" && pass==""){
+      this.Check=false
+      window.alert("Enter the username and password")
+    }
+    else if(name==this.a && pass==this.b ){
+      var Check=true
+      console.log(this.Check)
+       localStorage.setItem('loggedin','yes')
+      // console.log(this.Check);
+    }
+    else{
+      this.Check=false
+      window.alert("Invalid username or password")
+    }
+    console.log(this.Check+"check")
+  } 
+  canlogin():boolean{
+    return this.Check
+  }
+}
