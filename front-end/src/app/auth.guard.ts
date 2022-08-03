b77d25f8e7d27d7d6b935e46041c56b7f7c7965f
unknown <santhoshyavd@gmail.com>
2022-08-03 22:05:56 +0530

    changes

--- front-end/src/app/auth.guard.ts
+++ front-end/src/app/auth.guard.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { BformComponent } from './components/bform/bform.component';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  constructor(private auth:AuthService,private route:Router){
+class UserToken {}
+class Permissions {
+  canActivate(user: UserToken): boolean {
+    return true;
   }
-  canActivate(){
-    return true
-  }  
-
 }
+export class AuthGuard implements CanActivate {
+  constructor(private permissions: Permissions, private currentUser: UserToken,private router : Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
+    if(this.permissions.canActivate(this.currentUser)){
+      return true
+    }
+    else{
+      this.router.navigate(['login'])
+      return false
+    }
+  }
+}
+// export class AuthGuard implements CanActivate {
+//   constructor(private auth:AuthService,private route:Router){
+//   }
+//   canActivate(){
+//     return true
+//   }  
+
+// }
